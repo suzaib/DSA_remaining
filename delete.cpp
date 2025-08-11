a3719a9bc97388f5ee71c10515ccb0fe5fda1b1d
Suzaib <suzaib.work@gmail.com>
2025-08-11 15:51:11 +0530

    save

--- delete.cpp
+++ delete.cpp
@@ -1,24 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void pattern(int n){
-    int cnt=0;
-    for(int i=1;i<=n;i++){
-        for(int k=1;k<=i;k++){
-            cout<<k<<"";
-            cnt=k;
+class Node{
+    public:
+        int data;
+        Node* next;
+
+        Node(int data){
+            this->data=data;
         }
-        for(int k=1;k<=(2*n-2*i);k++){
-            cout<<" ";
-        }
-        for(int k=cnt;k>=1;k--){
-            cout<<k<<"";
-        }
-        cout<<"\n";
+};
+Node* add(Node* head1,Node* head2){
+    if(!head1) return head2;
+    if(!head2) return head1;
+    string s1;
+    Node* curr1=head1;
+    while(curr1){
+        s1+=('0'+curr1->data);
+        curr1=curr1->next;
     }
+
+    string s2;
+    Node* curr2=head2;
+    while(curr2){
+        s2+=('0'+curr2->data);
+        curr2=curr2->next;
+    }
+    int x=stoi(s1);
+    int y=stoi(s2);
+    int z=x+y;
+    string s=to_string(z);
+    Node* head=new Node(s[0]-'0');
+    Node* curr=head;
+    Node* newNode;
+    for(int i=1;i<s.size();i++){
+        newNode=new Node(s[i]-'0');
+        curr->next=newNode;
+    }
+
 }
 int main(){
-
-    pattern(5);
     return 0;
 }
