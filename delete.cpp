114dd6b1a1175ada55afcd6ecbe911c5be7d8639
Suzaib <suzaib.work@gmail.com>
2025-08-11 17:22:36 +0530

    Initial Commit

--- delete.cpp
+++ delete.cpp
@@ -10,34 +10,13 @@ class Node{
             this->data=data;
         }
 };
+Node* revLL(Node* head){
+    if(!head || !head->next) return head;
+    
+}
 Node* add(Node* head1,Node* head2){
     if(!head1) return head2;
     if(!head2) return head1;
-    string s1;
-    Node* curr1=head1;
-    while(curr1){
-        s1+=('0'+curr1->data);
-        curr1=curr1->next;
-    }
-
-    string s2;
-    Node* curr2=head2;
-    while(curr2){
-        s2+=('0'+curr2->data);
-        curr2=curr2->next;
-    }
-    int x=stoi(s1);
-    int y=stoi(s2);
-    int z=x+y;
-    string s=to_string(z);
-    Node* head=new Node(s[0]-'0');
-    Node* curr=head;
-    Node* newNode;
-    for(int i=1;i<s.size();i++){
-        newNode=new Node(s[i]-'0');
-        curr->next=newNode;
-    }
-
 }
 int main(){
     return 0;
