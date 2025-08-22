052b828f6e5519a4a20644370ec54608533c36d5
Suzaib <suzaib.work@gmail.com>
2025-08-22 21:33:42 +0530

    heap.cpp added

---
+++ heap.cpp
@@ -0,0 +1,13 @@
+/*Heap is just a binary tree, a complete binary tree, with heap order property
+Complete binary tree is filled at all levels except the last one, please see some examples, it is not exactly what you think
+Remember a tree is always filled(just basic convention) from the left hand side, in the inOrder way
+This convention is highly important when we talk about complete binary tree
+Heap order property is of two types, max heap, min heap
+Max heap means that every child is smaller than the parent
+Min heap means that every child is bigger than the parent
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+}
