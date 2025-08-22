41b9b3a07501df05288af0f6a81112388ea9b8f7
Suzaib <suzaib.work@gmail.com>
2025-08-22 23:14:58 +0530

    save on 22aug 23:14

--- heap.cpp
+++ heap.cpp
@@ -12,10 +12,28 @@ Min heap means that every child is bigger than the parent
 /*
 Heap are only represented as trees for understanding purpose, but they must be implemented as array
 Reason? 
-Consider an array with first index as 0
+Consider an array with first index filled with a null value
+Now fill this array with the heap elements(start from level 0 of the corresponding binary tree, and start filling from left to right)
+We will notice a very nice property, if any node is at index i in the array, then its left child will be at index 2*i in the same array
+and its right index will be at 2*i+1 in the same array.
+Also if a node is at ith index in the array, its parent will be at the (i/2)th index in the array.
 */
 #include<bits/stdc++.h>
 using namespace std;
+
+
+//Insertion in a Heap
+//Insert the element in order(if a space is left at the last level, fill it first in order(left to right), otherwise fill in the next level(left to right))
+//Then keep swapping the element with its parent until correct heap structure is formed
+vector<int> insertInHeap(vector<int> &arr,int k){
+    int n=arr.size();
+    arr.push_back(k);
+    int parentIdx=n/2;
+    while(k>arr[parentIdx]){
+        swap(arr[k],arr[parentIdx]);
+        parentIdx=k
+    }
+}
 int main(){
 
 }
