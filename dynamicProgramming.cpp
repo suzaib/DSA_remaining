f0dfedf0fcaf400e7e5c0e3c4f97fe6262007d6f
Suzaib <suzaib.work@gmail.com>
2025-09-25 15:58:40 +0530

    New

--- dynamicProgramming.cpp
+++ dynamicProgramming.cpp
@@ -1357,7 +1357,16 @@ int noOfSubsetsWithSumK_tabulation(vector<int> &arr,int k){
     vector<vector<int>> dp(n,vector<int> (k+1,0));
     for(int i=0;i<n;i++) dp[i][0]=1;
     dp[0][arr[0]]=1;
-    if(arr[0]=0) 
+    if(arr[0]=0) dp[0][0]=2;
+    for(int i=1;i<n;i++){
+        for(int j=1;j<=k;j++){
+            int pick=0;
+            if(arr[i]<=j) pick=dp[i-1][j-arr[i]];
+            int notPick=dp[i-1][j];
+            dp[i][j]=(pick+notPick);
+        }
+    }
+    return dp[n-1][k];
 }
 //DP On Strings
 
