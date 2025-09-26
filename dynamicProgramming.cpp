16aff471cd4ed9bded7039f652c2ab93f669dfc8
Suzaib <suzaib.work@gmail.com>
2025-09-26 19:56:15 +0530

    new

--- dynamicProgramming.cpp
+++ dynamicProgramming.cpp
@@ -1397,9 +1397,24 @@ int noOfSubsetsWithSumK_spaceOptimization(vector<int> &arr,int k){
 int noOfSubsetsWithSumK(vector<int> &arr,int k){
     int n=arr.size();
     vector<int> dp(k+1,0);
-    dp[0]=
+    dp[0]=1;
+    if(arr[0]<=k) dp[arr[0]]=1;
+    if(arr[0]==0) dp[0]=2;
+    for(int i=1;i<n;i++){
+        for(int j=k;j>=1;j--){
+            int pick=0;
+            if(arr[i]<=j) pick=dp[j-arr[i]];
+            int notPick=dp[j];
+            dp[j]=(pick+notPick);
+        }
+    }
+    return dp[k];
 }
+//Time Complexity will be O(NK)
+//Space Complexity will be O(K)
 
+
+//
 //DP On Strings
 
 //Longest Common Subsequence
