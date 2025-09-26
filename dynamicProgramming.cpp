59afa314cf989b5328f33b8c63787a971273c92e
Suzaib <suzaib.work@gmail.com>
2025-09-27 03:42:28 +0530

    new

--- dynamicProgramming.cpp
+++ dynamicProgramming.cpp
@@ -2208,9 +2208,7 @@ int lengthOfLIS_tabulation(vector<int> &arr){
     for(int idx=n-1;idx>=0;idx--){
         for(int prevIdx=idx-1;prevIdx>=-1;prevIdx--){
             int len=0+dp[idx+1][prevIdx+1];
-            if(prevIdx==-1 || arr[idx]>arr[prevIdx]){
-                len=max(len,1+dp[idx+1][idx+1]);
-            }
+            if(prevIdx==-1 || arr[idx]>arr[prevIdx]) len=max(len,1+dp[idx+1][idx+1]);
             dp[idx][prevIdx+1]=len;
         }
     }
