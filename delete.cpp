6c646df28f743372dade532fe75083d531602f2a
Suzaib <suzaib.work@gmail.com>
2025-09-28 03:27:04 +0530

    new

--- delete.cpp
+++ delete.cpp
@@ -5,8 +5,30 @@ int main(){
     return 0;
 }
 
-int fx(vector<int> &arr){
-    int n=arr.size();
-    vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
-    for(int i=)
+bool partitionAllowed(string &str,int start,int end){
+    while(start<=end){
+        if(str[start++]!=str[end--]) return false;
+    }
+    return true;
+}
+
+void partitionHelper(int idx,string &str,vector<string> &temp,vector<vector<string>> &ans){
+    if(idx==str.size()){
+        ans.push_back(temp);
+        return;
+    }
+    for(int i=idx;i<str.size();i++){
+        if(partitionAllowed(str,idx,i)){
+            path.push_back(str.substr(idx,i-idx+1));
+            partitionHelper(i,str,temp,ans);
+            path.pop_back();
+        }
+    }
+}
+
+vector<vector<string>> palindromePartition(string &str){
+    vector<vector<string>> ans;
+    vector<string> temp;
+    partitionHelper(temp,res);
+    return ans;
 }
