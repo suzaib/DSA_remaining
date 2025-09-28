db661a912540d2be4d2187b5c8ead755aa4fa8c4
Suzaib <suzaib.work@gmail.com>
2025-09-28 16:04:10 +0530

    new

--- delete.cpp
+++ delete.cpp
@@ -5,25 +5,20 @@ int main(){
     return 0;
 }
 
-void f(int n,vector<int> &temp,vector<bool> &vis,vector<int> &arr,vector<vector<int>> &ans){
-    if(temp.size()==n){
-        ans.push_back(temp);
+void f(int idx,int n,vector<int> &arr,vector<vector<int>> &ans){
+    if(idx==n){
+        ans.push_back(arr);
         return;
     }
-    for(int i=0;i<n;i++){
-        if(vis[i]) continue;
-        vis[i]=true;
-        temp.push_back(arr[i]);
-        f(n,temp,vis,arr,ans);
-        temp.pop_back();
-        vis[i]=false;
+    for(int i=idx;i<n;i++){
+        swap(arr[i],arr[idx]);
+        f(idx+1,n,arr,ans);
+        swap(arr[i],arr[idx]);
     }
 }
 vector<vector<int>> fx(vector<int> &arr){
     int n=arr.size();
     vector<vector<int>> ans;
-    vector<int> temp;
-    vector<bool> vis(n,false);
-    f(n,temp,vis,arr,ans);
+    f(0,n,arr,ans);
     return ans;
 }
