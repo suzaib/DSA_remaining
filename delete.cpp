2885820ede3c2f27e4f0483e72fb3875101caa9a
Suzaib <suzaib.work@gmail.com>
2025-10-03 04:30:17 +0530

    New

--- delete.cpp
+++ delete.cpp
@@ -4,17 +4,13 @@ using namespace std;
 int main(){
     return 0;
 }
-int f(int i,int j,string &s1,string &s2,vector<vector<int>> &dp){
-    if(i<0 || j<0) return 0;
-    if(dp[i][j]!=-1) return dp[i][j];
-    if(s1[i]==s2[j]) return dp[i][j]=(1+f(i-1,j-1,s1,s2,dp));
-    int ls=f(i-1,j,s1,s2,dp);
-    int rs=f(i,j-1,s1,s2,dp);
-    return dp[i][j]=max(ls,rs);
-}
 int fx(string &s1,string &s2){
     int n=s1.size();
     int m=s2.size();
-    vector<vector<int>> dp(n,vector<int> (m,-1));
-    return f(n-1,m-1,s1,s2,dp);
+    vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+        }
+    }
 }
