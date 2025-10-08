e29cb1b67e2ce1fec6a177a6f1edba324bfc5158
Suzaib <suzaib.work@gmail.com>
2025-10-08 11:27:49 +0530

    New

--- delete.cpp
+++ delete.cpp
@@ -4,13 +4,10 @@ using namespace std;
 int main(){
     return 0;
 }
+
 int fx(string &s1,string &s2){
     int n=s1.size();
     int m=s2.size();
-    vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-        }
-    }
+    vector<vector<int>> dp(n,vector<int>(m,-1));
+    return f(n-1,m-1,s1,s2,dp);
 }
