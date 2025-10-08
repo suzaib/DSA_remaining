24351624df7928aae8e6a043616d8ff7757f1b7c
Suzaib <suzaib.work@gmail.com>
2025-10-08 11:50:20 +0530

    New

--- delete.cpp
+++ delete.cpp
@@ -8,6 +8,14 @@ int main(){
 int fx(string &s1,string &s2){
     int n=s1.size();
     int m=s2.size();
-    vector<vector<int>> dp(n,vector<int>(m,-1));
-    return f(n-1,m-1,s1,s2,dp);
+    vector<int> prev(m+1,0);
+    vector<int> curr(m+1,0);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1[i-1]==s2[j-1]) curr[j]=1+prev[j-1];
+            else curr[j]=max(curr[j-1],prev[j]);
+        }
+        prev=curr;
+    }
+    return prev[m];
 }
