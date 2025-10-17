037e3b88e6c3de02a656a8c8800452bb19c55766
Suzaib <suzaib.work@gmail.com>
2025-10-18 04:40:36 +0530

    New

--- dynamicProgramming.cpp
+++ dynamicProgramming.cpp
@@ -2981,6 +2981,24 @@ int minOperationsInMCM_memoization(vector<int> &arr){
 //Space Complexity will be O(n2)+O(n)
 
 //Tabulation
+int minOperationsInMCM_tabulation(vector<int> &arr){
+    int n=arr.size();
+    vector<vector<int>> dp(n,vector<int> (n,0));
+    for(int i=n-1;i>=1;i--){
+        for(int j=i+1;j<n;j++){
+            int mini=1e9;
+            for(int k=i;k<j;k++){
+                int steps=arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
+                mini=min(mini,steps);
+            }
+            dp[i][j]=mini;
+        }
+    }
+    return dp[1][n-1];
+}
+//Time Complexity will be O(n3)
+//Space Complexity will be O(n2)
+
 //DP 30
 //DP 44 
 
