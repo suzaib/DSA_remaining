004e569720f62ef53b561a79fabab284da46b5ce
Suzaib <suzaib.work@gmail.com>
2025-11-05 15:22:19 +0530

    New

--- delete.cpp
+++ delete.cpp
@@ -17,49 +17,7 @@ int fx(vector<int> &arr){
 //Median of two sorted arrays
 //Brute Force
 //Merge two sorted arrays
-double med(vector<int> &a,vector<int> &b){
-    int n=a.size();
-    int m=b.size();
-    int s=n+m;
-    int cnt1=(s/2)-1;
-    int cnt2=(s/2);
-    int i=0;
-    int j=0;
-    int cnt=0;
-    int el1;
-    int el2;
-    while(i<n && j<m){
-        if(cnt==cnt1){
-            el1=min(a[i],b[j]);
-        }
-        if(cnt==cnt2){
-            el2=min(a[i],b[j]);
-        }
-        if(a[i]>b[j]){
-            j++;
-        }
-        else i++;
-        cnt++;
-    }
-    while(i<n){
-        if(cnt>s/2) break;
-        if(cnt==cnt1) el1=a[i];
-        if(cnt==cnt2) el2=a[i];
-        i++;
-        cnt++;
-    }
-    while(j<m){
-        if(cnt>s/2) break;
-        if(cnt==cnt1) el1=b[j];
-        if(cnt==cnt2) el2=b[j];
-        j++;
-        cnt++;
-    }
 
-    int ans;
-    if(s%2==0) return (el1+el2)/2.0;
-    return el2;
-}
 
 int main(){
     return 0;
