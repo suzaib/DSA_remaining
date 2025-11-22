5a26b62b50c21832529644ef9d91b7bd657a656e
Suzaib <suzaib.work@gmail.com>
2025-11-22 13:21:42 +0530

    new

--- delete.cpp
+++ delete.cpp
@@ -3,17 +3,26 @@ using namespace std;
 
 
 //Count Inversions
-int fx(vector<int> &arr){
-    int n=arr.size();
-    int cnt=0;
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[j]<arr[i]) cnt++;
-        }
+int conquer(int start,int mid,int end,vector<int> &arr){
+    int i=start;
+    int j=mid+1;
+    vector<int> temp;
+    while(i<=mid && j<=end){
+        if(arr[i]<arr[j]) temp.push_back(arr[i++]);
+        else temp.push_back(arr[j++]);
     }
-    return cnt;
 }
-
+int divide(int start,int end,vector<int> &arr){
+    if(start==end) return 0;
+    int mid=(start+end)>>1;
+    divide(start,mid,arr);
+    divide(mid+1,end,arr);
+    conquer(start,mid,end,arr);
+}
+int cntInversion(vector<int> &arr){
+    int n=arr.size();
+    return divide(0,n-1,arr);
+}
 //Median of two sorted arrays
 //Brute Force
 //Merge two sorted arrays
