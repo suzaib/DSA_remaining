7e9d3e45520a06eaf87115b0f8fa0141da29b3a5
ghost <ghost@kali.kali>
2025-12-22 02:45:05 +0530

    new

--- graphs.cpp
+++ graphs.cpp
@@ -1481,6 +1481,14 @@ vector<pair<int,int>> primsAlgorithm(int n,vector<vector<pair<int,int>>> &adj){
 //Space Complexity will be O(3V+E)
 
 
+//Disjoint Set
+//Consider a graph with two components(disconnected from each other), as 1->2->3 and 5->6
+//If someone asks us to check if 1 and 2 are connected the only option we have is to traverse from 1 and see if we reach 5 or not
+//However this takes too much time O(V+E)
+//Disjoint Set concepts allows us to do so in constant time
+//Disjoint Sets are used for dynamic graphs which keeps on changing with time
+//Disjoint Sets gives two functionalities, one is to find the parent the other is to find the union(can be done in two methods, rank or size)
+
 //Optimal Method for detect cycle in directed graph
 //Use a single visited array, you can mark 2 for path visited and 1 for visited
 
