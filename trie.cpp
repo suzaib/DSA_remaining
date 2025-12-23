8496de6db3f41113fff7bc3fbe963ec2256f0728
ghost <ghost@kali.kali>
2025-12-24 03:56:39 +0530

    new

--- trie.cpp
+++ trie.cpp
@@ -29,6 +29,11 @@ using namespace std;
 class Node{
     Node* links[26]={nullptr};
     bool flag=false;
+    bool containsChar(char ch) return links[ch-'a']!=NULL;
+    void put(char ch,Node* node) links[ch-'a']=node;
+    Node* get(char ch) return links[ch-'a'];
+    bool setEnd() flag=false;
+    bool isEnd() return flag;
 }
 class Trie{
     private:
@@ -42,16 +47,41 @@ class Trie{
         //Insert a word into the trie
         void insert(string word){
             Node* node=root;
-            for(int i=0;i<word.size();i++){}
+            for(int i=0;i<word.size();i++){
+                if(!node->containsChar(word[i])){
+                    node->put(word[i],new Node());
+                }
+
+                //Moves to the reference trie
+                node->get(word[i]);
+            }
+
+            node->setEnd();
         }
+        //If n is the length of word, then code will run for n times
+        //Time Complexity will be O(n)
 
         //Returns if a word is in the trie
         bool search(string word){
+            Node* node=root;
+            for(int i=0;i<word.size();i++){
+                if(!node->containsChar(word[i])) return false;
+                node=node->get(word[i])
+            }
 
+            return node->isEnd();
         }
+        //Time Complexity will be O(n)
 
         //Returns if there is a word in the trie that starts with the given prefix
         bool startsWith(string prefix){
+            Node* node=root;
+            for(int i=0;i<prefix.size();i++){
+                if(!node->containsChar(word[i])) return false;
+                node=node->get(word[i]);
+            }
 
+            return true;
         }
+        //Time Complexity will be O(n)
 }
