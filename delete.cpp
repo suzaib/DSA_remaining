6ae3e305081b28edaa9e59f9ae6a827a3b462d5b
ghost <ghost@kali.kali>
2025-12-25 19:46:07 +0530

    new

--- delete.cpp
+++ delete.cpp
@@ -1,12 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int helper(int idx,int prevIdx,int n,vector<int> &arr){
-    
-}
 int lis(vector<int> &arr){
     int n=arr.size();
-    return helper(0,-1,n,arr);
+    vector<int> dp(n,1);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(arr[j]>arr[i]) continue;
+            dp[i]=max(dp[i],1+dp[j]);
+        }
+    }
+    return (*(max_element(dp.begin(),dp.end())));
 }
 int main(){
     vector<int> arr={1,3,5};
