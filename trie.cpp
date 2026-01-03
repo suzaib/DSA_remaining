a5e75a23dcd338056c974ed5bb96a5238d171ae1
Suzaib <suzaib.work@gmail.com>
2026-01-03 19:03:15 +0530

    new

--- trie.cpp
+++ trie.cpp
@@ -449,32 +449,15 @@ class Node{
 int distSubstr(string &s){
     int n=s.size();
     Node* root=new Node();
-    Node* node=root;
     int cnt=0;
     for(int i=0;i<n;i++){
         Node* node=root;
         for(int j=i;j<n;j++){
-            if(!node->containsChar(s[i])){
+            if(!node->containsChar(s[j])){
                 cnt++;
-                node->put(s[i],new Node());
+                node->put(s[j],new Node());
             }
-            node=node->get(s[i]);
-        }
-    }
-}
-
-int fx(string &s){
-    int n=s.size();
-    Node* root=new Node();
-    int cnt=0;
-    for(int i=0;i<n;i++){
-        Node* node=root;
-        for(int j=i;j<n;j++){
-            if(!node->containsChar(s[i])){
-                cnt++;
-                node->put(s[i],new Node());
-            }
-            node=node->get(s[i]);
+            node=node->get(s[j]);
         }
     }
     return cnt;
