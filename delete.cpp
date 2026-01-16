46c35f43d06d63616efa8ed5c1a2c3a625b8900b
Suzaib <suzaib.work@gmail.com>
2026-01-16 15:07:15 +0530

    new

--- delete.cpp
+++ delete.cpp
@@ -1,46 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int upperBound(vector<int> &arr,int x){
-    int n=arr.size();
-    int low=0;
-    int high=n-1;
-    while(low<=high){
-        int mid=(low+high)>>1;
-        if(arr[mid]>x) high=mid-1;
-        else low=mid+1;
-    }
-    return low;
-}
-int blackbox(vector<vector<int>> &mat,int mid){
-    int n=mat.size();
-    int m=mat[0].size();
-    int cnt=0;
-    for(int i=0;i<n;i++){
-        cnt+=upperbound(mat[i],mid);
-    }
-    return cnt;
-}
+class Node{
+    public:
+        Node* next;
+        Node* prev;
 
-int median(vector<vector<int>> &mat){
-    int n=mat.size();
-    int m=mat[0].size();
-    int low=INT_MAX;
-    int high=INT_MIN;
-    for(int i=0;i<n;i++){
-        low=min(low,arr[i][0]);
-        high=max(high,arr[i][m-1]);
-    }
+        int val;
 
-    int req=(n*m)/2;
+        Node(int val){
+            this->head=nullptr;
+            this->prev=nullptr;
+            this->val=val;
+        }
 
-    while(low<=high){
-        int mid=(low+high)>>1;
-        int lesserThanOrEqualTo=blackbox(mat,mid);
-        if(lesserThanOrEqualTo>req) high=mid-1;
-        else low=mid+1;
-    }
-    return low;
 }
 int main(){
     vector<int> arr={1,3,5};
