eb02c22a2ee6edcdfc4adf6748c3c95ed4f27cba
Suzaib <suzaib.work@gmail.com>
2026-01-19 22:09:32 +0530

    new

--- delete.cpp
+++ delete.cpp
@@ -1,5 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+void srtHelper(int prev,stack<int> st){
+    if(st.empty()) return prev;
+    int curr=st.top();
+    st.pop();
+    srtHelper(curr,st);
+    
+}
+stack<int> srtStack(stack<int> st){
+    int curr=st.top();
+    st.pop();
+    stSrtHelper(curr,st);
+}
 
 
 int main(){
