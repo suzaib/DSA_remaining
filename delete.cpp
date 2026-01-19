42be010bd85b1f25005b10b749e325f27770c602
Suzaib <suzaib.work@gmail.com>
2026-01-19 15:13:40 +0530

    new

--- delete.cpp
+++ delete.cpp
@@ -1,20 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
-        Node* next;
-        Node* prev;
 
-        int val;
-
-        Node(int val){
-            this->head=nullptr;
-            this->prev=nullptr;
-            this->val=val;
-        }
-
-}
 int main(){
     vector<int> arr={1,3,5};
     vector<int> lows(1)
