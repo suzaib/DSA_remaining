3595341414d67d375391304d1fc2a5a0ef62763d
suzaib <suzaib.work@gmail.com>
2026-02-12 03:08:59 +0530

    new

---
+++ recurrenceRelation&MasterMethod.cpp
@@ -0,0 +1,12 @@
+/*
+Here we discuss the Master Method which is used to find the time complexity of the recurrence relations
+For that we first need to discuss recurrence relations and how to find the time complexity 
+We are already much aware of what a recurrence relation is, therefore we will try to find the time complexity of some recurrence relation
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    return 0;
+}
