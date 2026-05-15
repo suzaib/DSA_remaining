6aa13ae3bd1bbb458156617551f093cd497387b6
suzaib <suzaib.work@gmail.com>
2026-05-16 05:06:04 +0530

    New

---
+++ differenceArray.cpp
@@ -0,0 +1,6 @@
+/*
+Difference Array Technique solves some similar problems and hence may be confused with the segment trees
+However they are still very much different from the segment trees
+*/
+
+
