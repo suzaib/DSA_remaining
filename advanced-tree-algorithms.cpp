b45cfe94ea528e8d565bef46ada11760bc598e6c
Suzaib <suzaib.work@gmail.com>
2026-06-12 05:39:46 +0530

    New

---
+++ advanced-tree-algorithms.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Binary Lifting
+
+/*
+Finding the kth ancestor 
+Sample Question from the CSES problem set
+https://cses.fi/problemset/task/1687
+The kth ancestor means for eg if k=1 the ancestor would be the element just above
+
+This thing is new because remember, trees are not like doubly linked lists where we can move upward
+The brute force is finding kth child for all nodes and see if any of them is the mentioned node
+We will write down the brute force below
+*/
+
+//Brute Force
+//Find the kth child
+int findKthAncestor_brute(Node* root){
+
+}
+int main(){
+    //Your function here
+    return 0;
+}
